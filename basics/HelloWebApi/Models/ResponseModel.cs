2ab51d427ff9e0222858153b4226ed2cb16fd5ce
senaboyuktas <senabykts99@gmail.com>
2024-02-08 17:31:19 +0300

    Add project files.

---
+++ basics/HelloWebApi/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWebApi.Models
+{
+    public class ResponseModel
+    {
+        public int HttpStatus { get; set; }
+        public String Message { get; set; }
+    }
+}
---
+++ basics/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet] // information sevyesinde olduğu için log kaydı düşmüyor -> IIS de çalışırken 
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product() {Id = 1, ProductName="Computer"},
+                new Product() {Id = 2, ProductName="Keyboard"},
+                new Product() {Id = 3, ProductName="Mouse"}
+            };
+            _logger.LogInformation("GetAllProducts action has been called.");
+            return Ok(products);
+        }
+
+        [HttpPost] // production modda olduğu için log kaydı düşüyor -> IIS de çalışırken 
+        public IActionResult GetAllProducts([FromBody] Product product)
+        {
+            _logger.LogWarning("Product has been created.");
+            return StatusCode(201); // Created.
+        }
+
+        // Development modda her iki kısımda da log kaydı düşüyor
+    }
+}
